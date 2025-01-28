0ee70a475caece31f13bc799aa9b32a7fafdd1c3
hariharan.moorthy <hariharan.moorthy@CBE-DLT-343>
2025-01-28 17:21:09 +0530

    Adder user login and register with authentication

--- ToDoApp/ToDoApp/Program.cs
+++ ToDoApp/ToDoApp/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ToDoApp
+﻿using ToDoApp.Manager;
+using ToDoApp.Repository;
+using ToDoApp.UserInteraction;
+
+namespace ToDoApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            UserRepository userRepository = new UserRepository();
+            LoginInteraction loginInteraction = new LoginInteraction(userRepository);
+            LoginManager loginManager = new LoginManager(loginInteraction);
+            loginManager.LoginMenu();
+            Console.ReadKey();
         }
     }
 }
